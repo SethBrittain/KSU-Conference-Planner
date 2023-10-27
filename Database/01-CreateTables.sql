09ff6cb3aed8f0cd4bd616f7f0454e58d005fa41
enbeatty <85527023+enbeatty@users.noreply.github.com>
2023-10-27 15:38:50 +0000

    updated tables

--- Database/01-CreateTables.sql
+++ Database/01-CreateTables.sql
@@ -1,22 +1,17 @@
--- Create attendee_types table
-CREATE TABLE IF NOT EXISTS public.attendee_types
-(
-    attendee_type_id smallserial NOT NULL,
-    attendee_type_name character varying COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT attendee_types_pkey PRIMARY KEY (attendee_type_id, attendee_type_name)
-);
+CREATE TYPE attendee_type AS ENUM ('attendee', 'reviewer', 'scheduler')
 
 -- Create event_registrations table
 CREATE TABLE IF NOT EXISTS public.event_registrations
 (
-    registration_id bigserial NOT NULL,
-    person_id bigint NOT NULL,
+    event_registration_id bigserial NOT NULL,
+    user_id bigint NOT NULL,
     event_id bigint NOT NULL,
-	attendee_type_id bigint NOT NULL,
+	attendee_type attendee_type NOT NULL,
     expires_on timestamp with time zone NOT NULL DEFAULT (CURRENT_TIMESTAMP + '6 mons'::interval),
     created_on timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_on timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT event_registration_pkey PRIMARY KEY (registration_id)
+    CONSTRAINT event_registration_pkey PRIMARY KEY (registration_id),
+    UNIQUE( user_id, event_id)
 );
 
 -- Create events table
@@ -24,17 +19,19 @@ CREATE TABLE IF NOT EXISTS public.events
 (
     event_id bigserial NOT NULL,
     name character varying COLLATE pg_catalog."default" NOT NULL,
-    start_date timestamp with time zone NOT NULL,
-	end_date timestamp with time zone NOT NULL,
+    first_date date NOT NULL,
+	last_date date NOT NULL,
     created_on timestamp with time zone NOT NULL,
     updated_on timestamp with time zone NOT NULL,
-    CONSTRAINT events_pkey PRIMARY KEY (event_id)
+    conference_id bigint NOT NULL,
+    CONSTRAINT events_pkey PRIMARY KEY (event_id),
+    UNIQUE(first_date, last_date, conference_id)
 );
 
 -- Create people table
-CREATE TABLE IF NOT EXISTS public.people
+CREATE TABLE IF NOT EXISTS public.user
 (
-    person_id bigserial NOT NULL,
+    user_id bigserial NOT NULL PRIMARY KEY,
     email character varying(128) COLLATE pg_catalog."default" NOT NULL,
     first_name character varying(64) COLLATE pg_catalog."default" NOT NULL,
     last_name character varying(64) COLLATE pg_catalog."default" NOT NULL,
@@ -42,23 +39,26 @@ CREATE TABLE IF NOT EXISTS public.people
     salt bigint,
     dietary_restrictions character varying(4096) COLLATE pg_catalog."default",
     accommodations character varying(4096) COLLATE pg_catalog."default",
-    CONSTRAINT people_pkey PRIMARY KEY (person_id, email)
+    created_on timestamp with time zone NOT NULL,
+    updated_on timestamp with time zone NOT NULL
+    UNIQUE(email)
 );
 
 -- Create presentation_assignments table
 CREATE TABLE IF NOT EXISTS public.presentation_assignments
 (
-    assignment_id bigserial NOT NULL,
-    registration_id bigint NOT NULL,
+    presentation_assignment_id bigserial NOT NULL PRIMARY KEY,
+    user_id bigint NOT NULL,
     presentation_id bigint NOT NULL,
     review boolean NOT NULL DEFAULT false,
-    CONSTRAINT presentation_assignment_pkey PRIMARY KEY (assignment_id, registration_id, presentation_id)
+    created_on timestamp with time zone NOT NULL,
+    updated_on timestamp with time zone NOT NULL
 );
 
--- Create presentations table
-CREATE TABLE IF NOT EXISTS public.presentations
+-- Create presentation table
+CREATE TABLE IF NOT EXISTS public.presentation
 (
-    presentation_id bigserial NOT NULL,
+    presentation_id bigserial NOT NULL PRIMARY,
     attendee_id bigint NOT NULL,
     presentation_name character varying(255) COLLATE pg_catalog."default" NOT NULL DEFAULT 'Untitled Presentation'::character varying,
     authors character varying COLLATE pg_catalog."default" NOT NULL,
@@ -66,17 +66,21 @@ CREATE TABLE IF NOT EXISTS public.presentations
     technical_requirements character varying COLLATE pg_catalog."default",
     presentation_type character varying COLLATE pg_catalog."default" NOT NULL,
     accepted boolean NOT NULL DEFAULT false,
-    CONSTRAINT presentations_pkey PRIMARY KEY (presentation_id)
+    created_on timestamp with time zone NOT NULL,
+    updated_on timestamp with time zone NOT NULL
 );
 
 -- Create sessions table
 CREATE TABLE IF NOT EXISTS public.sessions
 (
-    session_id bigserial NOT NULL,
+    session_id bigserial NOT NULL PRIMARY KEY,
     presentation_id bigint NOT NULL,
     event_id bigint NOT NULL,
     session_start_time timestamp with time zone NOT NULL,
     session_end_time timestamp with time zone NOT NULL,
-    room_number character varying(16) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT sessions_pkey PRIMARY KEY (session_id, event_id, presentation_id)
+    room_id bigint NOT NULL,
+    room_number character varying(16) COLLATE pg_catalog."default" NOT NULL, --Move this to room table
+    created_on timestamp with time zone NOT NULL,
+    updated_on timestamp with time zone NOT NULL,
+    UNIQUE(event_id, session_start_time, session_end_time, room_id)
 );
