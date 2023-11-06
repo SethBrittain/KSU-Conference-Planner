eb6e60684e5b586c481bb937129456c3f2e10b91
enbeatty <85527023+enbeatty@users.noreply.github.com>
2023-11-06 15:52:22 +0000

    small updates to navbar and landing page

--- Frontend/src/Components/Navbar/Navbar.tsx
+++ Frontend/src/Components/Navbar/Navbar.tsx
@@ -1,93 +1,51 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Button } from "../Button/Button";
+import { Button } from "./Button";
+import "./style.css";
 
-type NavBarProps = {
-  Navstyle: "sessions" | "FAQ" | "home" | "about" | "sign-up";
-  className: any;
-}
-
-export const NavbarsGeneric = ({ Navstyle, className }: NavBarProps): JSX.Element => {
+export const NavbarsGeneric = ({ navStyle, className }) => {
   return (
-    <div className={`w-[1440px] h-[100px] bg-white ${className}`}>
-      <div className="h-[100px] bg-white relative">
-        <div
-          className={`inline-flex absolute ${Navstyle === "sign-up" ? "left-[165px]" : "left-[1221px]"} ${
-            Navstyle === "sign-up" ? "items-start" : "items-center"
-          } ${Navstyle === "sign-up" ? "top-[38px]" : "top-[29px]"} ${Navstyle === "sign-up" ? "gap-[40px]" : "gap-[22px]"}`}
-        >
-          {Navstyle === "sign-up" && (
+    <div className={`navbars-generic ${className}`}>
+      <div className="header">
+        <div className={`menu-items ${navStyle}`}>
+          {navStyle === "sign-up" && (
             <>
-              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#637381] text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-                Home
-              </div>
-              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#637381] text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-                Sessions
-              </div>
-              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#637381] text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-                About
-              </div>
-              <div className="relative w-fit mt-[-1.00px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#637381] text-[16px] tracking-[0] leading-[24px] whitespace-nowrap">
-                FAQ
-              </div>
+              <div className="text-wrapper">Home</div>
+              <div className="text-wrapper">Sessions</div>
+              <div className="text-wrapper">About</div>
+              <div className="text-wrapper">FAQ</div>
             </>
           )}
 
-          {["FAQ", "about", "home", "sessions"].includes(Navstyle) && (
+          {["FAQ", "about", "home", "sessions"].includes(navStyle) && (
             <>
-              <div className="[font-family:'Inter-Medium',Helvetica] w-fit tracking-[0] text-[16px] text-[#111928] font-medium leading-[24px] whitespace-nowrap relative">
-                Sign In
-              </div>
+              <div className="login">Sign In</div>
               <Button
-                buttonText="Sign Up"
-                className="!flex-[0_0_auto] !px-[24px] !py-[9px] !bg-[#906397]"
-                color="primary"
-                kind="primary"
-                round="semi-round"
+                className="button-instance"
+                icon="none"
+                size="base"
                 state="default"
+                text="Sign Up"
+                type="primary"
               />
             </>
           )}
         </div>
-        {["FAQ", "about", "home", "sessions"].includes(Navstyle) && (
-          <div className="left-[165px] inline-flex items-start top-[38px] gap-[40px] absolute">
-            <div
-              className={`[font-family:'Inter-Medium',Helvetica] w-fit mt-[-1.00px] tracking-[0] text-[16px] font-medium leading-[24px] whitespace-nowrap relative ${
-                Navstyle === "home" ? "text-[#111928]" : "text-[#637381]"
-              }`}
-            >
-              Home
-            </div>
-            <div
-              className={`[font-family:'Inter-Medium',Helvetica] w-fit mt-[-1.00px] tracking-[0] text-[16px] font-medium leading-[24px] whitespace-nowrap relative ${
-                Navstyle === "sessions" ? "text-[#111928]" : "text-[#637381]"
-              }`}
-            >
-              Sessions
-            </div>
-            <div
-              className={`[font-family:'Inter-Medium',Helvetica] w-fit mt-[-1.00px] tracking-[0] text-[16px] font-medium leading-[24px] whitespace-nowrap relative ${
-                Navstyle === "about" ? "text-[#111928]" : "text-[#637381]"
-              }`}
-            >
-              About
-            </div>
-            <div
-              className={`[font-family:'Inter-Medium',Helvetica] w-fit mt-[-1.00px] tracking-[0] text-[16px] font-medium leading-[24px] whitespace-nowrap relative ${
-                Navstyle === "FAQ" ? "text-[#111928]" : "text-[#637381]"
-              }`}
-            >
-              FAQ
-            </div>
+        {["FAQ", "about", "home", "sessions"].includes(navStyle) && (
+          <div className="div">
+            <div className={`text-wrapper-2 nav-style-${navStyle}`}>Home</div>
+            <div className={`text-wrapper-3 nav-style-0-${navStyle}`}>Sessions</div>
+            <div className={`text-wrapper-4 nav-style-1-${navStyle}`}>About</div>
+            <div className={`text-wrapper-5 nav-style-2-${navStyle}`}>FAQ</div>
           </div>
         )}
 
-        <div className="w-[79px] left-[43px] top-[11px] h-[79px] rounded-[39.5px] bg-[#906397] absolute" />
+        <div className="ellipse" />
       </div>
     </div>
   );
 };
 
 NavbarsGeneric.propTypes = {
-  Navstyle: PropTypes.oneOf(["sessions", "FAQ", "home", "about", "sign-up"]),
+  navStyle: PropTypes.oneOf(["sessions", "FAQ", "home", "about", "sign-up"]),
 };
--- Frontend/src/Components/Pages/LandingPage/LandingPage.tsx
+++ Frontend/src/Components/Pages/LandingPage/LandingPage.tsx
@@ -1,59 +1,58 @@
 import React from "react";
-import { Button } from "../../Button/Button";
-import { MainInfo } from "../../MainInfo/MainInfo";
-import { NavbarsGeneric } from "../../Navbar/Navbar";
-import { PhotosAndInfo } from "../../PhotosAndInfo/PhotosAndInfo";
+import { Button } from "./Button";
+import { MainInfo } from "./MainInfo";
+import { NavbarsGeneric } from "./NavbarsGeneric";
+import { PhotosAndInfo } from "./PhotosAndInfo";
+import "./style.css";
 
-export const LandingPage = (): JSX.Element => {
+export const LandingPage = () => {
   return (
-    <div className="bg-[#ffffff] flex flex-row justify-center w-full">
-      <div className="bg-[#ffffff] w-[1440px] h-[2236px] relative">
-        <div className="absolute w-[1440px] h-[304px] top-[1932px] left-0 bg-[#d9d9d9]" />
-        <div className="absolute w-[1440px] h-[1932px] top-0 left-0">
-          <div className="flex flex-col w-[1440px] h-[1872px] items-center gap-[30px] absolute top-[60px] left-0">
-            <div className="flex h-[376px] items-end justify-end gap-[10px] p-[10px] relative self-stretch w-full bg-[#d9d9d9]">
+    <div className="landing-page">
+      <div className="div-2">
+        <div className="rectangle-2" />
+        <div className="overlap-group">
+          <div className="frame-3">
+            <div className="frame-4">
               <Button
-                className="!h-[88px] !mb-[-3.00px] !flex !bg-[#906397] !w-[251px]"
                 icon="none"
-                size="xl"
-                state="focus"
+                size="xll"
+                state="default"
                 text="Current Sessions"
-                textClassName="!mr-[-13.00px] !tracking-[0] !ml-[-19.00px] !text-[28px] ![font-style:unset] !font-semibold ![font-family:'Inter-SemiBold',Helvetica] !leading-[42px]"
+                textClassName="design-component-instance-node"
                 type="primary"
               />
               <Button
-                className="!h-[88px] !mr-[-3.00px] !mb-[-3.00px] !flex !bg-[#906397] !w-[251px]"
                 icon="none"
-                size="xl"
-                state="focus"
+                size="xll"
+                state="default"
                 text="Join The Event"
-                textClassName="!mr-[-11.00px] !tracking-[0] !ml-[-17.00px] !text-[32px] ![font-style:unset] !font-semibold ![font-family:'Inter-SemiBold',Helvetica] !leading-[48px]"
+                textClassName="button-2"
                 type="primary"
               />
             </div>
-            <div className="flex flex-wrap w-[1422px] items-center justify-between gap-[10px_10px] px-[72px] py-[10px] relative flex-[0_0_auto]">
+            <div className="frame-5">
               <MainInfo />
               <MainInfo />
               <MainInfo />
             </div>
-            <div className="flex flex-col items-center gap-[10px] p-[10px] relative flex-1 self-stretch w-full grow">
+            <div className="frame-6">
               <PhotosAndInfo
-                className="!self-stretch !h-[291px] !flex !w-full"
+                className="photos-and-info-instance"
                 inverted={false}
-                rectangleClassName="!self-stretch !h-[unset] !flex-1 !grow !w-[unset]"
-                sampleTextSampleClassName="!self-stretch !flex-1 !w-[unset]"
+                rectangleClassName="photos-and-info-2"
+                sampleTextSampleClassName="photos-and-info-3"
                 text="Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text&nbsp;&nbsp;Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text&nbsp;&nbsp;Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text"
               />
               <PhotosAndInfo
-                className="!self-stretch !h-[291px] !flex !w-full"
+                className="photos-and-info-instance"
                 inverted
-                rectangleClassName="!self-stretch !flex-1 !w-[unset]"
-                sampleTextSampleClassName="!self-stretch !h-[unset] !flex-1 !grow !w-[unset]"
+                rectangleClassName="photos-and-info-3"
+                sampleTextSampleClassName="photos-and-info-2"
                 text="Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text&nbsp;&nbsp;Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text&nbsp;&nbsp;Sample text Sample text Sample text Sample text Sample text Sample text&nbsp;&nbsp;text Sample text Sample text Sample text"
               />
             </div>
           </div>
-          <NavbarsGeneric className="!absolute !left-0 !top-0" Navstyle='about' />
+          <NavbarsGeneric className="navbars-generic-instance" headerClassName="navbars-generic-2" style="home" />
         </div>
       </div>
     </div>
