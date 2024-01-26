587a5d4ca9eed5f4e4b48687cb204a095ec376ef
Sam <89217609+mcqualit@users.noreply.github.com>
2024-01-26 13:07:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ docker compose down
 docker volume rm ksu-conference-planner_postgres
 docker build --file ./Dockerfile . -t sethbrittain/conferenceplanner:1.0.0
 docker compose up -d
+npm install @mui/material @emotion/react @emotion/styled
