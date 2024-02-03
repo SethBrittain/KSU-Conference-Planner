24c9596ed7d85f2572c9b963cb8fc354da228997
Sam <89217609+mcqualit@users.noreply.github.com>
2024-02-03 00:40:54 +0000

    Forgot to upload files oops

---
+++ Frontend/src/Components/NavbarScheduler/NavbarScheduler.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AppBar, IconButton, Toolbar, Typography, Stack, Button, Box} from "@mui/material";
+
+const NavbarScheduler = () =>
+{
+    return(
+        <Box display="flex" flexDirection="row">
+        <AppBar position='static'style={{ background: '#FFFFFF'}}>
+            <Toolbar >
+                <Typography variant='h5' component='div' style={{ color: '#000000'}} sx={{ flexGrow: 0, m: 2}}>
+                KSU Conference Planner
+                </Typography>
+                <Stack direction ='row' spacing = {2} sx={{ flexGrow: 1  }}>
+                    <Button style={{ color: '#000000'}} >Home</Button>
+                    <Button style={{ color: '#000000'}}>Sessions</Button>
+                    <Button style={{ color: '#000000'}}>About</Button>
+                    <Button style={{ color: '#000000'}}>FAQ</Button>
+                    <Button style={{ color: '#000000'}}>Your Schedule</Button>
+                    <Button style={{ color: '#000000'}}>Conference Planner</Button>
+                </Stack>
+                <Stack direction ='row' spacing = {2} sx={{ flexGrow: 0  }}>
+                    <Button style={{ color: '#FFFFFF', backgroundColor: '#512888'}}>Log Out</Button>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+        </Box>
+        
+    )
+}
+
+export default NavbarScheduler;
