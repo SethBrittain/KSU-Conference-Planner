3cb49121b19c81d9c6d4a7b4b1ce93a878dfc931
enbeatty <85527023+enbeatty@users.noreply.github.com>
2024-03-01 18:50:43 +0000

    Continue Sign Up Page 2

--- Frontend/src/Components/Pages/SignUpPage2/SignUpPage2.tsx
+++ Frontend/src/Components/Pages/SignUpPage2/SignUpPage2.tsx
@@ -2,7 +2,7 @@ import NavbarGeneric from '../../NavbarGeneric/NavbarGeneric';
 import {Stack, TextField} from '@mui/material';
 import React, { useLayoutEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { FormControl, FormLabel } from '@mui/material';
+import { FormControl, FormLabel, Radio, RadioGroup, FormControlLabel, Grid } from '@mui/material';
 
 type SignUpPage2Props = {
     text: string;
@@ -53,17 +53,44 @@ const SignUpPage2 = (): React.JSX.Element => {
                 gap={2}
                 p={8}
                 flexDirection="column"
-                sx={{ justifyContent: 'center' }}
+                sx={{ justifyContent: 'center', bgcolor: '#FFFFFF'}}
                 >
+                    <Box
+                    color='#512888'
+                    fontSize={40}
+                    textAlign='center'
+                    >
+                    Sign Up
+                    </Box>
                     <FormControl defaultValue="" required>
-                        <div>
-                            <FormLabel>Enter Stuff</FormLabel>
-                            <TextField></TextField>
-                        </div>
-                        <div>
-                            <FormLabel>Enter Stuff</FormLabel>
-                            <TextField></TextField>
-                        </div>
+                        <FormLabel>First Name</FormLabel>
+                        <TextField></TextField>
+                        <FormLabel>Last Name</FormLabel>
+                        <TextField></TextField>
+                            
+                        <FormLabel id="attendeeType">Attendee Type</FormLabel>
+                        <RadioGroup
+                            aria-labelledby="attendeeType"
+                            defaultValue="undergrad"
+                            name="attendeeTypeGroup"
+                        >
+                            <Grid container spacing={2} >
+                            <FormControlLabel 
+                            value="undergrad" 
+                            control={<Radio name="ID" style={{ paddingTop: '20px' }}/>} 
+                            label="K-State Undergrad Student" 
+                            />
+                            <FormControlLabel 
+                            value="grad" 
+                            control={<Radio name="ID" />} 
+                            label="K-State Grad Student"
+                             />
+                            <FormControlLabel value="alumni" control={<Radio />} label="K-State Alumni" />
+                            <FormControlLabel value="faculty" control={<Radio />} label="K-State Faculty" />
+                            <FormControlLabel value="teacher" control={<Radio />} label="High School Teacher" />
+                            </Grid>
+                            
+                        </RadioGroup>
                     </FormControl>
                 </Box>
                 
