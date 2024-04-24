d7d06804b6996166af4bc3e819848fed0804f489
enbeatty <85527023+enbeatty@users.noreply.github.com>
2024-04-24 18:00:23 +0000

    navbar comments

--- Frontend/src/Components/Navbars/Navbar.tsx
+++ Frontend/src/Components/Navbars/Navbar.tsx
@@ -1,10 +1,8 @@
 import React, { ReactElement, useState } from "react";
-import { AppBar, Toolbar, Typography, Stack, Button, Box} from "@mui/material";
+import { Button } from "@mui/material";
 import {ReactComponent as Icon} from '../Icon.svg';
-import { Link } from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
 import './Navbar.css';
-import RouteConfig from '../../RouteConfig';
 import UserRole from "../../Utils/UserRole";
 
 type NavbarItem = {
@@ -17,6 +15,11 @@ type NavbarProps = {
 	userRole : UserRole
 }
 
+/**
+ * This function is to make a populated array with current items
+ * @param user the current users role
+ * @returns the navbar items that should be shown
+ */
 function MakeNavItems( user: UserRole ): NavbarItem[] {
 	var navItems:NavbarItem[] = [];
 	var home:NavbarItem = {name: "Home", shown: true, path: "/"};
@@ -28,11 +31,13 @@ function MakeNavItems( user: UserRole ): NavbarItem[] {
 	var applicationReview:NavbarItem = {name: "Application Review", shown: true, path: "/review"};
 	var scheduler:NavbarItem = {name: "Scheduler", shown: true, path: "/scheduler"};
 	
+	//These will always be shown
 	navItems.push( home );
 	navItems.push( sessions );
 	navItems.push( about );
 	navItems.push( faq );
 
+	//By going through this switch the list will be populated with more items if needed
 	switch (user){
 		case UserRole.Admin:
 			navItems.push( yourSchedule );
@@ -59,20 +64,35 @@ function MakeNavItems( user: UserRole ): NavbarItem[] {
 	return navItems;
 }
 
+/**
+ * This checks the current path you are on to see if sign in button should be shown
+ * @param user the current users role
+ * @returns Null if element is not shown, button if it is shown
+ */
 function SignItem( user: UserRole ): ReactElement | null {
-    if(user === UserRole.Anonymous && document.location.pathname !== "/signup"){ //TODO Page Address
+    if(user === UserRole.Anonymous && document.location.pathname !== "/signup"){ //The Page Address may need to change
         return <a href="/signIn"> <Button style={{ color: '#000000', fontSize: '.8rem'}} >Sign In</Button></a>;
     }
     return null;
 }
 
+/**
+ * This checks the current path you are on to see if sign up button should be shown
+ * @param user the current users role
+ * @returns Null if element is not shown, button if it is shown
+ */
 function SignUpItem(user: UserRole): ReactElement | null {
-    if(user === UserRole.Anonymous && document.location.pathname !== "/signup"){ //TODO Page Address
+    if(user === UserRole.Anonymous && document.location.pathname !== "/signup"){ //The Page Address may need to change
         return <a href="/signup1"><Button style={{ color: '#FFFFFF', backgroundColor: '#512888', fontSize: '.8rem'}}>Sign Up</Button></a>;
     }
     return null;
 }
 
+/**
+ * This checks the role you are to see if logout button should be shown
+ * @param user the current users role
+ * @returns Null if element is not shown, button if it is shown
+ */
 function LogOutItem(user: UserRole): ReactElement | null {
     if(user === UserRole.Anonymous){
         return null;
@@ -80,9 +100,11 @@ function LogOutItem(user: UserRole): ReactElement | null {
     return <Button style={{ color: '#FFFFFF', backgroundColor: '#512888'}}>Sign Out</Button>;
 }
 
-
-
-
+/**
+ * How the navbar should be shown get made here
+ * @param userRole this is of type UserRole
+ * @returns the navbar component
+ */
 const Navbar = ( {userRole}: NavbarProps) =>
 {
 	const [show, setShow] = useState(false);
@@ -91,6 +113,7 @@ const Navbar = ( {userRole}: NavbarProps) =>
 
 	const navItems : NavbarItem[] = MakeNavItems(userRole);
 
+	//Loops through to check the pathname so that if you are on the page it will bold it
 	navItems.forEach((item)=>{
 		NavBarItems.push(<a href={item.path} className={
 			item.shown ? (
@@ -105,7 +128,7 @@ const Navbar = ( {userRole}: NavbarProps) =>
 		setShow(!show);
 	}
 
-	const newVersion : React.JSX.Element =
+	const nav : React.JSX.Element =
 		<div id="navbar" className="flex flex-col md:flex-row bg-white drop-shadow-lg items-center sticky top-0 py-4">
 			<div className="flex items-center justify-between w-full md:w-auto">
 				<Icon/>
@@ -125,7 +148,7 @@ const Navbar = ( {userRole}: NavbarProps) =>
 			</div>
 		</div>;
 
-    return newVersion;
+    return nav;
 }
 
 export default Navbar;
