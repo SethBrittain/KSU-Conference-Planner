69ee286b30d0dfb5f7713624659f479a9751a780
Sam <89217609+mcqualit@users.noreply.github.com>
2024-04-29 17:14:53 +0000

    Commented Photos and Info

--- Frontend/src/Components/PhotosAndInfo/PhotosAndInfo.tsx
+++ Frontend/src/Components/PhotosAndInfo/PhotosAndInfo.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import placeholder from "./31343C.svg";
 
+/**
+ * Information need to create the component
+ */
 type PhotosAndInfoProps = {
     image: string;
     text: string;
@@ -8,17 +11,24 @@ type PhotosAndInfoProps = {
     size: number;
     side: boolean;
 }
-
+/**
+ * Creates the component for photos and info
+ * @param PhotosAndInfoProps The information needed to create the component  
+ * @returns The component for PhotosAndInfo
+ */
 const PhotosAndInfo = ({ image, text, spacing, size, side }: PhotosAndInfoProps) => {
-    const [loaded, setLoaded] = useState(false);
-
+    //The state of the image
+	const [loaded, setLoaded] = useState(false);
+	//Sets loaded to true
 	const isLoaded = () => setLoaded(true);
 
 	return (
 		<div className="border border-gray-200 flex flex-col shadow-lg h-full">
 			<div className="flex align-center justify-center overflow-hidden grow">
+				{/*The image of the component */}
 				<img style={{content: loaded ? "" : "url('"+placeholder+"')"}} src={image} onLoad={isLoaded} className="min-w-full min-h-full shrink-0"/>
 			</div>
+			{/*The text part of the component */}
 			<p className="py-4 p-2 text-lg">
 				{text}
 			</p>
